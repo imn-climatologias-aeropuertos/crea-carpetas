7ebc97f9a35c767de030f5cd4ee46d868aa6b600
diego-garro <diego.garromolina@yahoo.com>
2026-04-21 17:19:29 -0600

    refactor: sigwx models and functions

--- src/download/sigwx.py
+++ src/download/sigwx.py
@@ -1,16 +1,40 @@
 import asyncio
 import os
 
+from abc import ABC
 from pathlib import Path
 
 import requests
 
-SIGWX_BASE_AMERICAS_URL = (
-    "https://aviationweather.gov/data/products/swh/XX_sigwx_hi_a.gif"
-)
-SIGWX_BASE_EURO_AFR_URL = (
-    "https://aviationweather.gov/data/products/swh/XX_sigwx_hi_b1.gif"
-)
+
+class AviationWeatherSigwxUrl(ABC):
+    _base = "https://aviationweather.gov/data/products/swh/XX_sigwx_hi_REGION.gif"
+    _americas = "a"
+    _ames_africa = "b1"
+
+    @property
+    def americas(self):
+        return self._base.replace("REGION", self._americas)
+
+    @property
+    def ames_africa(self):
+        return self._base.replace("REGION", self._ames_africa)
+
+
+class TurkishHezarfenSigwxUrl(ABC):
+    _base = "https://hezarfen.mgm.gov.tr/Genel/imgKrtPng.ashx?cevir=CEVIR&syol=REGION_XX00.png"
+    _americas = "PGEE07"
+    _ames_africa = "PGSE06"
+
+    @property
+    def americas(self):
+        return self._base.replace("CEVIR", "0").replace("REGION", self._americas)
+
+    @property
+    def ames_africa(self):
+        return self._base.replace("CEVIR", "1").replace("REGION", self._ames_africa)
+
+
 SAVE_DATA_PATH = "./storage/img/maps/"
 MEAN_SYNOP_HOURS = ["00", "06", "12", "18"]
 
@@ -24,26 +48,43 @@ def create_dirs():
 
 def download_sigwx_maps_sync(base_url: str, region: str):
     create_dirs()
-    try:
-        for hour in MEAN_SYNOP_HOURS:
-            url = base_url.replace("XX", hour)
-            response = requests.get(url)
-            response.raise_for_status()
+    url = AviationWeatherSigwxUrl()
 
-            output_path = SAVE_DATA_PATH + f"{hour}Z_SIGWX_{region.upper()}.jpg"
-            with open(output_path, "wb") as f:
-                f.write(response.content)
+    for hour in MEAN_SYNOP_HOURS:
+        url = base_url.replace("XX", hour)
+        response = requests.get(url)
+        response.raise_for_status()
 
-            print(f"Imagen guardada en: {output_path}.")
-    except requests.exceptions.RequestException as e:
-        print(f"Error al descargar la imagen: {e}")
+        output_path = SAVE_DATA_PATH + f"{region.upper()}_{hour}Z_SIGWX.jpg"
+        with open(output_path, "wb") as f:
+            f.write(response.content)
+
+        print(f"Imagen guardada en: {output_path}.")
 
 
 async def download_sigwx_maps_async(need_europe_maps: bool):
-    await asyncio.to_thread(
-        download_sigwx_maps_sync, SIGWX_BASE_AMERICAS_URL, "americas"
-    )
-    if need_europe_maps:
+    try:
         await asyncio.to_thread(
-            download_sigwx_maps_sync, SIGWX_BASE_EURO_AFR_URL, "euroafr"
+            download_sigwx_maps_sync, AviationWeatherSigwxUrl().americas, "americas"
         )
+        if need_europe_maps:
+            await asyncio.to_thread(
+                download_sigwx_maps_sync,
+                AviationWeatherSigwxUrl().ames_africa,
+                "ames_africa",
+            )
+    except requests.exceptions.RequestException as e:
+        try:
+            await asyncio.to_thread(
+                download_sigwx_maps_sync, TurkishHezarfenSigwxUrl().americas, "americas"
+            )
+            if need_europe_maps:
+                await asyncio.to_thread(
+                    download_sigwx_maps_sync,
+                    TurkishHezarfenSigwxUrl().ames_africa,
+                    "ames_africa",
+                )
+        except requests.exceptions.RequestException as e:
+            raise Exception(
+                f"Error al descargar los mapas de tiempo significante de AviationWeather y Hezarfen: {e}"
+            )
